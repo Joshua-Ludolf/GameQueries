ecdc9c162a602a85008bdfa8d77404c654d14b4c
s550turbo <Benunderdahl@gmail.com>
2023-11-27 00:44:47 -0600

    created the routes and connected the search link to the search.html page

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run
+
+
+
+
